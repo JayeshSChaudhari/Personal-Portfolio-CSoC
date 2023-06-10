703c6b73a6aef9f86d96834f440938d43b768566
JayeshSChaudhari <chaudhari.jayesh.cd.mec22@itbhu.ac.in>
2023-06-11 00:04:54 +0530

    New JS File

---
+++ script.js
@@ -0,0 +1,53 @@
+var Rot_Welcome = document.querySelector('#Sidebar p');
+
+
+
+
+
+
+setTimeout(() => {
+    document.querySelector('#Sidebar p').classList.add('Title_Round');
+}, 1000);
+
+setTimeout(() => {
+    document.querySelector('.BasicInfo').classList.add('Tilt_Info');
+}, 3000);
+
+setTimeout(() => {
+    document.querySelector('.Interests').classList.add('Animate_Interest');
+}, 5000);
+
+setTimeout(() => {
+    document.querySelector('.Skills').classList.add('Tilt_Skills');
+}, 7000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
